d201e1d1937cecd937824570b7be2548de38eecc
Miguel Castro <miguel.castro@nearseatech.io>
2023-04-03 15:03:12 +0100

    fix: add quotes

--- remove_images.sh
+++ remove_images.sh
@@ -1,3 +1,3 @@
-TAGS=$(doctl registry repository lt $1 --format Tag,UpdatedAt | tail -n +2 | sort -rk2 | awk '{print $1}' | grep -v -E $3 | tail -n +$2 | tr '\n' ' ')
+TAGS=$(doctl registry repository lt $1 --format Tag,UpdatedAt | tail -n +2 | sort -rk2 | awk '{print $1}' | grep -v -E "$3" | tail -n +$2 | tr '\n' ' ')
 [[ -n $TAGS ]] && doctl registry repository dt $1 $TAGS --force
 echo Tags removed: $TAGS
