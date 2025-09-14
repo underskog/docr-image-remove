7e240481d19b898cf9f1a6bd8d40521cac00a878
henrik242 <henrik242@users.noreply.github.com>
2025-09-14 10:03:15 +0200

    Reorder parameters in delete-manifest command

--- remove_images.sh
+++ remove_images.sh
@@ -1,6 +1,6 @@
 DIGESTS=$(doctl registry repository lm $1 --format Digest,UpdatedAt,Tags | tail -n +2 | sort -rk2 | grep -v -E "$3" | awk '{print $1}' | tail -n +$(($2+1)))
 for DIGEST in $DIGESTS; do
-  doctl registry repository delete-manifest $1 $DIGEST --force
+  doctl registry repository delete-manifest --force $1 $DIGEST
 done
 echo Manifest Digests removed: $DIGESTS
 
