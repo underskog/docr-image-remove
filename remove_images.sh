517deaa9447b093f5066f6c801a030af6e6ce115
henrik242 <henrik242@users.noreply.github.com>
2025-09-14 10:07:51 +0200

    Add some debug

--- remove_images.sh
+++ remove_images.sh
@@ -1,7 +1,11 @@
 DIGESTS=$(doctl registry repository lm $1 --format Digest,UpdatedAt,Tags | tail -n +2 | sort -rk2 | grep -v -E "$3" | awk '{print $1}' | tail -n +$(($2+1)))
 for DIGEST in $DIGESTS; do
-  doctl registry repository delete-manifest --force $1 $DIGEST
+  CMD="doctl registry repository delete-manifest --force $1 $DIGEST"
+  echo Running $CMD
+  $CMD
 done
 echo Manifest Digests removed: $DIGESTS
 
-doctl registry garbage-collection start --include-untagged-manifests --force
+CMD="doctl registry garbage-collection start --include-untagged-manifests --force"
+echo Running $CMD
+$CMD
